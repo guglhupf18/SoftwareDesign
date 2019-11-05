142fb793bfc091f1778edb21b4f658a9c7b632aa
guglhupf18 <46997627+guglhupf18@users.noreply.github.com>
2019-11-05 10:24:01 +0100

    Added XOX-Program

---
+++ XOX/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace XOX_Test02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            generatePlayfield();
+            printBoardAndTurn();
+
+        }
+        static string[] board;
+        static int turn;
+        static bool win = false;
+        static void generatePlayfield()
+        {
+            turn = 0;
+            board = new string[9];
+            board[0] = "0";
+            board[1] = "1";
+            board[2] = "2";
+            board[3] = "3";
+            board[4] = "4";
+            board[5] = "5";
+            board[6] = "6";
+            board[7] = "7";
+            board[8] = "8";
+            
+        }
+
+        static void printBoardAndTurn()
+        {
+            Console.WriteLine(board[0] + "" + board[1] + "" + board[2]);
+            Console.WriteLine(board[3] + "" + board[4] + "" + board[5]);
+            Console.WriteLine(board[6] + "" + board[7] + "" + board[8]);
+
+            checkForWin();
+
+            if (turn < 9 && !win)
+                Console.WriteLine("Nächster Spieler ist am Zug");
+
+            else if (turn == 9 && !win)
+                Console.WriteLine("Das Spiel endet unentschieden");
+
+            if (win)
+                Console.WriteLine("Das Spiel wurde gewonnen");
+
+
+            while (!win)
+            {   
+                var input = Console.ReadLine();
+                playerTurn(Int32.Parse(input));
+
+            }
+
+        }
+
+        static void playerTurn(int position)
+        {
+            Console.WriteLine("turn" + turn);
+            if (turn/2 < 1)
+                board[position] = "X";
+            else
+                board[position] = "Y";
+
+            turn++;
+            printBoardAndTurn();
+
+
+
+        }
+        static bool checkForWin()
+        {
+            if (board[0] == board[1] && board[0] == board[2])
+                win = true;
+
+            else if (board[3] == board[4] && board[3] == board[5])
+                win = true;
+
+            else if (board[6] == board[7] && board[6] == board[8])
+                win = true;
+            else if (board[0] == board[4] && board[0] == board[8])
+                win = true;
+
+            else if (board[2] == board[4] & board[2] == board[6])
+                win = true;
+
+            else
+                win = false;
+
+            return win;
+        }
+
+    }
+}
