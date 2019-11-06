27101ed10867ec7261e8cb80de6c7feedde7f056
guglhupf18 <46997627+guglhupf18@users.noreply.github.com>
2019-11-06 01:39:23 +0100

    quiz added

---
+++ L04_Quiz/QuizElement.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace L04_Quiz
+{
+    class QuizElement
+    {
+        public string question = "";
+        public List<string> answers = new List<string>();
+        public int correctAnswer = 0;
+
+
+    }
+}
