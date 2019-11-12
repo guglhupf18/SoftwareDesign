045fb0daba2be2c64dd195a0c72371f39db2bc54
guglhupf18 <46997627+guglhupf18@users.noreply.github.com>
2019-11-12 09:40:32 +0100

    Quizaction

--- SoftwareDesign-Praktikum-Projektmappe/L04_Quiz/QuizElement.cs
+++ SoftwareDesign-Praktikum-Projektmappe/L04_Quiz/QuizElement.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Text;
 
+namespace L04_Quiz
+{
+    
     class QuizElement
     {
         public string question = "";
         public List<string> answers = new List<string>();
         public int correctAnswer = 0;
 
-
     }
-
+}
