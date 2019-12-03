84c98a80c1905a1fdf72374c5eb6e0cbd5ba52c2
guglhupf18 <46997627+guglhupf18@users.noreply.github.com>
2019-12-03 10:30:08 +0100

    changed vsroject

---
+++ SoftwareDesign-Praktikum-Projektmappe/L06_GenericTree/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace L06_GenericTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ SoftwareDesign-Praktikum-Projektmappe/L06_GenericTree/TreeNode.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace L06_GenericTree
+{
+    class TreeNode<T>
+    {
+        TreeNode<T> parent;
+        List<TreeNode<T>> children;
+        TreeNode<T> root;
+
+        public TreeNode<T> CreateNode(TreeNode<T> node)
+        {
+            return node;
+        }
+        public void AppendChild(TreeNode<T> child)
+        {
+            parent = this;
+            children.Add(child);            
+                      
+        }
+
+        public void RemoveChild(TreeNode<T> node)
+        {
+            children.Remove(node);
+ 
+        }
+
+        public void PrintTee()
+        {
+            Console.WriteLine("Parent: " + parent);
+            for (int i = 0; i < children.Count; i++)
+                Console.WriteLine("Children" + children[i] + "\n");
+            Console.WriteLine("Root:" + root);
+        }
+    }
+}
